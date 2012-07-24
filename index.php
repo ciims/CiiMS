9fee9e1ae8fa0ad3faf512769c8506913af97626
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-07-24 09:15:24 -0500

    Re-added index.php file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// change the following paths if necessary
+$yii='YII_PATH';
+$config=dirname(__FILE__).'/protected/config/main.php';
+// remove the following lines when in production mode
+//defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+if (file_exists('install.php')) 
+{
+	require_once('install.php');
+	exit();
+}
+require_once($yii);
+Yii::createWebApplication($config)->run();
+
