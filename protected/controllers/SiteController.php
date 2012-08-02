f2a71825b8e31bd780f2cd065c3cfdd24a0e4b73
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2012-08-02 10:05:35 -0500

    Removed drafts in all views

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -70,7 +70,7 @@ class SiteController extends CiiController
 			$criteria->addInCondition('id', array_keys(isset($result['matches']) ? $result['matches'] : array()));
 			$criteria->addCondition("vid=(SELECT MAX(vid) FROM content WHERE id=t.id)");
 			$criteria->addCondition('password = ""');
-			
+			$criteria->addCondition('status = 1');
 			$criteria->limit = $pageSize;	
 			$criteria->order = 'id DESC';		
 			$itemCount = Content::model()->count($criteria);
@@ -102,7 +102,7 @@ class SiteController extends CiiController
 			$criteria->addSearchCondition('title', $_GET['q'], true, 'OR');
 			$criteria->addCondition("vid=(SELECT MAX(vid) FROM content WHERE id=t.id)");
 			$criteria->addCondition('password = ""');
-			
+			$criteria->addCondition('status = 1');
 			$criteria->limit = $pageSize;	
 			$criteria->order = 'id DESC';		
 			$itemCount = Content::model()->count($criteria);
