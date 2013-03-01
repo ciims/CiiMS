fa9e25cd684834f7e34ae0f1bf916f11284d578e
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-03-01 13:03:05 -0600

    Added PHPUnit Support

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
 
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../../frameworks/php/yii/framework/yiit.php';
+$yiit=dirname(__FILE__).'/../runtime/yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
+$CiiMSTestConfig = require_once($config);
 require_once($yiit);
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
-Yii::createWebApplication($config);
+Yii::createWebApplication($CiiMSTestConfig);
---
+++ protected/tests/unit/BaseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class BaseTest extends CTestCase
+{
+    public function testUnitTesting()
+    {
+        echo 'Unit Tests Active...';
+        $this->assertTrue(true);
+    }   
+}
