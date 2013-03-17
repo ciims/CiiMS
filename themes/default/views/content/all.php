abe02604920ef41fdd981d1a7610799d2ff0fa06
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-03-16 22:09:22 -0500

    Update to all

--- themes/default/views/content/all.php
+++ themes/default/views/content/all.php
@@ -3,12 +3,27 @@
     	<?php $this->renderPartial('//content/_post', array('content' => $content)); ?>
     <?php endforeach; ?>
 </div>
-<?php $this->widget('ext.yiinfinite-scroll.YiinfiniteScroller', array(
-    'url'=>isset($url) ? $url : 'blog',
-    'contentSelector' => '#posts',
-    'itemSelector' => 'div.post',
-    'loadingText' => 'Loading...',
-    'donetext' => '&nbsp;',
-    'pages' => $pages,
-)); ?>
-<?php Yii::app()->clientScript->registerScript('unbind-infinite-scroll', "$(window).unbind('.infscr');"); ?>
+
+<?php if (count($data)): ?>
+	<?php $this->widget('ext.yiinfinite-scroll.YiinfiniteScroller', array(
+	    'url'=>isset($url) ? $url : 'blog',
+	    'contentSelector' => '#posts',
+	    'itemSelector' => 'div.post',
+	    'loadingText' => 'Loading...',
+	    'donetext' => '&nbsp;',
+	    'pages' => $pages,
+	)); ?>
+	<?php Yii::app()->clientScript->registerScript('unbind-infinite-scroll', "$(window).unbind('.infscr');"); ?>
+<?php else: ?>
+	<div class="post">
+		<?php if (Cii::get(Cii::get($meta, 'blog-image', array()), 'value', '') != ""): ?>
+			<p style="text-align:center;"><?php echo CHtml::image(Yii::app()->baseUrl . $meta['blog-image']['value'], NULL, array('class'=>'image')); ?></p>
+		<?php endif; ?>
+		<div class="post-inner">
+			<div class="post-header">
+				<h3>There are no posts in this category.</h3>
+			</div>
+		</div>
+	    <div style="clear:both;"><br /></div>
+	</div>
+<?php endif; ?>
