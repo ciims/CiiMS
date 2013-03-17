b71a668b83f826045fd078db75237cf8f2d00191
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-03-16 23:43:14 -0500

    Fixing potential issue with content creation

--- protected/models/Content.php
+++ protected/models/Content.php
@@ -283,7 +283,9 @@ class Content extends CiiModel
      */
 	public function beforeValidate()
 	{        
-    	if ($this->isNewRecord)
+		// If this is a new record and we don't have a created data pre-populated
+		// Failing to check the $this->created results in posts losing their original creation date
+    	if ($this->isNewRecord && $this->created == NULL)
     	{
     		// Implicit flush to delete the URL rules
 			$this->created = new CDbExpression('NOW()');
