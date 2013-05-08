ad27f99ce29a7af5130a473b7d44f61d7a88639f
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-05-08 09:06:48 -0500

    Fixing yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,8 +1,9 @@
 <?php
-
 // change the following paths if necessary
-$configFile=dirname(__FILE__).'/config/main.php';
+$config=dirname(__FILE__).'/config/main.php';
 
-// Pull yiic path from
-$config = require_once($configFile);
+$config = require($config);
+require((string)$config['params']['yiiPath'].'yii.php');
+$config = CMap::mergeArray(require(dirname(__FILE__).'/config/main.default.php'), $config);
+
 require_once((string)$config['params']['yiiPath'].'yiic.php');
