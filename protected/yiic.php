9010dd3f178e8382ac0f31f34d7c5f65cd664fe2
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-05-08 09:10:57 -0500

    Updating yiic

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,9 +1,27 @@
 <?php
+function mergeArray($a,$b)
+{
+    $args=func_get_args();
+    $res=array_shift($args);
+    while(!empty($args))
+    {
+        $next=array_shift($args);
+        foreach($next as $k => $v)
+        {
+            if(is_integer($k))
+                isset($res[$k]) ? $res[]=$v : $res[$k]=$v;
+            elseif(is_array($v) && isset($res[$k]) && is_array($res[$k]))
+                $res[$k]=mergeArray($res[$k],$v);
+            else
+                $res[$k]=$v;
+        }
+    }
+    return $res;
+}
 // change the following paths if necessary
 $config=dirname(__FILE__).'/config/main.php';
 
 $config = require($config);
-require((string)$config['params']['yiiPath'].'yii.php');
-$config = CMap::mergeArray(require(dirname(__FILE__).'/config/main.default.php'), $config);
+$config = mergeArray(require(dirname(__FILE__).'/config/main.default.php'), $config);
 
 require_once((string)$config['params']['yiiPath'].'yiic.php');
