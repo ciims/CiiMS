d91f89727b9d0468bf1132072e2f2d30995889f2
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-05-12 11:00:16 -0500

    This may help with category saving

--- protected/models/Categories.php
+++ protected/models/Categories.php
@@ -125,17 +125,14 @@ class Categories extends CiiModel
 	public function beforeSave()
 	{
     	if ($this->isNewRecord)
-    	{
-    		Yii::app()->cache->delete('categories');
-			Yii::app()->cache->delete('WFF-categories-url-rules');
-			Yii::app()->cache->delete('categories-pid');
-			$this->created = new CDbExpression('NOW()');			
-			$this->updated = new CDbExpression('NOW()');
-		}
+			$this->created = new CDbExpression('NOW()');
 	   	else
 			$this->updated = new CDbExpression('NOW()');
 		
         Yii::app()->cache->delete('categories-listing');
+		Yii::app()->cache->delete('categories');
+		Yii::app()->cache->delete('WFF-categories-url-rules');
+		Yii::app()->cache->delete('categories-pid');
 	    return parent::beforeSave();
 	}
 	
