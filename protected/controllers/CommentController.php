ciims/CiiMS

commit 2de962aadd40f79ad7de7091ea834a42f6451217

Allow emails to be themeable and allow custom SMTP config

protected/controllers/CommentController.php
@@ -106,14 +106,33 @@ class CommentController extends CiiController
 				// Send an email to the author if someone makes a comment on their blog
 				if ($content->author->id != Yii::app()->user->id && Configuration::model()->findByAttributes(array('key'=>'notifyAuthorOnComment'))->value == 1) 
 				{
+					// Send email
+					$adminUser = Users::model()->findByPk(1);
 					Yii::import('application.extensions.phpmailer.JPhpMailer');
 					$mail = new JPhpMailer;
 					$mail->IsSMTP();
-					$mail->SetFrom('noreply@'. Cii::get($_SERVER, 'HTTP_HOST', Cii::get($_SERVER, 'SERVER_NAME', 'localhost')), 'No Reply');
+
+					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
+					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
+					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
+					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+
+					if ($smptHost !== NULL)
+						$mail->Host       = $smptHost->value; 
+
+					if ($smtpPort !== NULL)
+						$mail->Port       = $smtpPort->value;
+
+					if ($smtpUser !== NULL)                    
+						$mail->Username   = $smptUser->value; 
+
+					if ($smptPass !== NULL)
+						$mail->Password   = $smptPass->value;       
+
+					$mail->SetFrom($adminUser->email, $adminUser->name);
 					$mail->Subject = 'New Comment Notification From CiiMS Blog';
-					$mail->AltBody = 'New Comment on: ' . $content->title;
-					$mail->MsgHTML($this->renderPartial('/email/comments', array('content'=>$content, 'comment'=>$comment), true, true));
-					$mail->AddAddress($content->author->email, $content->author->displayName);
+					$mail->MsgHTML($this->renderPartial('//email/comments', array('content'=>$content, 'comment'=>$comment));
+					$mail->AddAddress($user->email, $user->displayName);
 					$mail->Send();
 				}
 

protected/controllers/SiteController.php
@@ -250,20 +250,33 @@ class SiteController extends CiiController
 					$meta->value = $expires;
 					$meta->save();
 					
-					// Create the message to be sent to the user
-					$message = "
-					{$user->displayName},<br /><br />
-					You recently notified us that you forgot your password. Don't worry, it happens to all of us. To reset your password, please " . CHtml::link('click here', Yii::app()->createAbsoluteUrl('/forgot/' . $hash)) .
-					".<br /><br />Thank you,<br />" . Yii::app()->name . 
-					" Team<br /><br />P.S. If you did not request this email, you may safely ignore it.";
-					
+
 					// Send email
+					$adminUser = Users::model()->findByPk(1);
 					Yii::import('application.extensions.phpmailer.JPhpMailer');
 					$mail = new JPhpMailer;
 					$mail->IsSMTP();
-					$mail->SetFrom('noreply@' . $_SERVER['HTTP_HOST'], Yii::app()->name . ' Administrator');
+
+					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
+					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
+					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
+					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+
+					if ($smptHost !== NULL)
+						$mail->Host       = $smptHost->value; 
+
+					if ($smtpPort !== NULL)
+						$mail->Port       = $smtpPort->value;
+
+					if ($smtpUser !== NULL)                    
+						$mail->Username   = $smptUser->value; 
+
+					if ($smptPass !== NULL)
+						$mail->Password   = $smptPass->value;       
+
+					$mail->SetFrom($adminUser->email, $adminUser->name);
 					$mail->Subject = 'Your Password Reset Information';
-					$mail->MsgHTML($message);
+					$mail->MsgHTML( $this->renderPartial('//email/forgot', array('user' => $user, 'hash' => $hash), true, true));
 					$mail->AddAddress($user->email, $user->displayName);
 					$mail->Send();
 					
@@ -422,21 +435,33 @@ class SiteController extends CiiController
 						$meta->key = 'activationKey';
 						$meta->value = $hash;
 						$meta->save();
-						
-						// Create the message to be sent to the user
-						$message = "
-						{$user->displayName},<br /><br />
-						Thanks for registering your account! To activate your account, " . CHtml::link('click here', Yii::app()->createAbsoluteUrl('/activation/'.$user->id.'/'.$hash)) .
-						".<br /><br />Thank you,<br />" . Yii::app()->name . 
-						" Team<br /><br />P.S. If you did not request this email, you may safely ignore it.";
-						
+											
 						// Send email
+						$adminUser = Users::model()->findByPk(1);
 						Yii::import('application.extensions.phpmailer.JPhpMailer');
 						$mail = new JPhpMailer;
 						$mail->IsSMTP();
-						$mail->SetFrom('noreply@' . $_SERVER['HTTP_HOST'], Yii::app()->name . ' Administrator');
+
+						$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
+						$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
+						$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
+						$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+
+						if ($smptHost !== NULL)
+							$mail->Host       = $smptHost->value; 
+
+						if ($smtpPort !== NULL)
+							$mail->Port       = $smtpPort->value;
+
+						if ($smtpUser !== NULL)                    
+							$mail->Username   = $smptUser->value; 
+
+						if ($smptPass !== NULL)
+							$mail->Password   = $smptPass->value;       
+
+						$mail->SetFrom($adminUser->email, $adminUser->name);
 						$mail->Subject = 'Activate Your Account';
-						$mail->MsgHTML($message);
+						$mail->MsgHTML( $this->renderPartial('//email/register', array('user' => $user, 'hash' => $hash), true, true));
 						$mail->AddAddress($user->email, $user->displayName);
 						$mail->Send();
 					

themes/default/views/email/forgot.php
@@ -1,0 +1,7 @@
+<?php echo $user->displayName; ?>,<br /><br />
+You recently notified us that you forgot your password for CiiMS Blog <?php echo Yii::app()->name; ?>. To reset your password, <?php echo CHtml::link('click here', Yii::app()->createAbsoluteUrl('/forgot/' . $hash)); ?> and follow the instructions on the reset page. This link is only valid for 15 minutes.
+<br /><br />
+Thank you,<br />
+<?php echo Yii::app()->name . " Team"; ?>
+<br /><br />
+P.S. If you did not request this email, you may safely ignore it.

themes/default/views/email/register.php
@@ -1,0 +1,7 @@
+<?php echo $user->displayName; ?>,<br /><br />
+Thanks for registering your account at CiiMS Blog <?php echo Yii::app()->name; ?>. To verify your account, <?php echo CHtml::link('click here', Yii::app()->createAbsoluteUrl('/activation/'.$user->id.'/'.$hash)); ?>. This action will verify your email address and allow you to interact with features only available to registered users.
+<br /><br />
+Thank you,<br />
+<?php echo Yii::app()->name . " Team"; ?>
+<br /><br />
+P.S. If you did not request this email, you may safely ignore it.

commit 186a2d101f39b9e47dedc71b92cf4cc0959b1bee

Fixes bug introduced by 2de962a. Applies to #26 and #27

protected/controllers/CommentController.php
@@ -112,10 +112,10 @@ class CommentController extends CiiController
 					$mail = new JPhpMailer;
 					$mail->IsSMTP();
 
-					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
-					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
-					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
-					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost'));
+					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort'));
+					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser'));
+					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass'));
 
 					if ($smptHost !== NULL)
 						$mail->Host       = $smptHost->value; 

protected/controllers/SiteController.php
@@ -257,10 +257,10 @@ class SiteController extends CiiController
 					$mail = new JPhpMailer;
 					$mail->IsSMTP();
 
-					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
-					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
-					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
-					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+					$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost'));
+					$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort'));
+					$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser'));
+					$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass'));
 
 					if ($smptHost !== NULL)
 						$mail->Host       = $smptHost->value; 
@@ -442,10 +442,10 @@ class SiteController extends CiiController
 						$mail = new JPhpMailer;
 						$mail->IsSMTP();
 
-						$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost');
-						$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort');
-						$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser');
-						$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass');
+						$smtpHost = Configuration::model()->findByAttributes(array('key' => 'SMTPHost'));
+						$smtpPost = Configuration::model()->findByAttributes(array('key' => 'SMTPPort'));
+						$smtpUser = Configuration::model()->findByAttributes(array('key' => 'SMTPUser'));
+						$smtpPass = Configuration::model()->findByAttributes(array('key' => 'SMTPPass'));
 
 						if ($smptHost !== NULL)
 							$mail->Host       = $smptHost->value; 