e784d0b5a9ddd03bb4d870c98e7e49dc77a79da0
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-05-23 20:04:09 -0500

    fixing social signin

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -94,7 +94,7 @@ class UserIdentity extends CUserIdentity
 		    // Return early if the record is NULL. Bad things seem to happen with the $meta if we don't =(
 		    return !$this->errorCode;
 		}
-		else if ($this->password == md5("PUBUSER"))
+		else if ($this->password == md5("PUBUSER") && !$this->force)
 		{
 			// This is a socially authenticated user who hasn't set a password. Do not allow them to login using the wildcard password
 			// If we can't find the user's email, return identity failure
