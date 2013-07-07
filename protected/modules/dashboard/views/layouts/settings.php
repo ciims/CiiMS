71da0432f83a5db152a095d7dbd12fdcb8192b47
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-07-07 10:27:20 -0500

    Behavior changes

--- protected/modules/dashboard/views/layouts/settings.php
+++ protected/modules/dashboard/views/layouts/settings.php
@@ -13,6 +13,7 @@
 					array('url' => $this->createUrl('/dashboard/categories'), 'label' => 'Categories', 'itemOptions' => array('class' => 'icon-list')),
 					array('url' => $this->createUrl('/dashboard/analytics/settings'), 'label' => 'Analytics', 'itemOptions' => array('class' => 'icon-bar-chart')),
 					array('url' => $this->createUrl('/dashboard/settings/appearance'), 'label' => 'Appearance', 'itemOptions' => array('class' => 'icon-eye-open')),
+					array('url' => $this->createUrl('/dashboard/settings/social'), 'label' => 'Social', 'itemOptions' => array('class' => 'icon-twitter')),
 					array('url' => $this->createUrl('/dashboard/settings/cards'), 'label' => 'Cards & Plugins', 'itemOptions' => array('class' => 'icon-puzzle-piece')),
 					array('url' => $this->createUrl('/dashboard/settings/cache'), 'label' => 'System Cache', 'itemOptions' => array('class' => 'icon-cloud')),
 				)
@@ -22,4 +23,9 @@
 			<?php echo $content; ?>
 		</div>
 	</div>
+	<?php Yii::app()->getClientScript()->registerScript('li-click', '
+		$(".menu li").click(function() { 
+			window.location = $(this).find("a").attr("href");
+		});
+	'); ?>
 <?php $this->endContent(); ?>
