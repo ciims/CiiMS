e42ee278a8ebd68d3c9ded56af9b53dd1039e48d
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-07-18 08:58:39 -0500

    Added marked.js and highlight.js

--- protected/modules/dashboard/views/content/save.php
+++ protected/modules/dashboard/views/content/save.php
@@ -3,38 +3,51 @@
 	<div class="editor">
 		<div id="main">
 			<div class="content">
+				<?php echo $form->textArea($model, 'content'); ?>
 			</div>
 		</div>
 	</div>
 	<div class="body-content">
 		<div id="main" class="nano">
 			<div class="content">
+				<div class="preview"></div>
 			</div>
 		</div>
 	</div>	
 </div>
 
 <?php $this->endWidget(); ?>
-<?php Yii::app()->getClientScript()->registerCss('form', 'form { height: 100%; }'); ?>
-<?php Yii::app()->getClientScript()->registerScriptFile($this->asset.'/js/jquery.nanoscroller.min.js', CClientScript::POS_END); ?>
-<?php Yii::app()->getClientScript()->registerScriptFile($this->asset.'/js/marked.js', CClientScript::POS_END); ?>
-<?php Yii::app()->getClientScript()->registerScript('nano-scroller', '
-		$(".nano").nanoScroller();
-'); ?>
-<?php Yii::app()->getClientScript()->registerScript('marked', '
-	marked.setOptions({
-	  gfm: true,
-	  highlight: function (code, lang, callback) {
-	    pygmentize({ lang: lang, format: "html" }, code, function (err, result) {
-	      callback(err, result.toString());
-	    });
-	  },
-	  tables: true,
-	  breaks: true,
-	  pedantic: false,
-	  sanitize: true,
-	  smartLists: true,
-	  smartypants: false,
-	  langPrefix: "lang-"
-	});
-'); ?>
+
+<?php $cs = Yii::app()->getClientScript(); ?>
+
+<?php
+	  $cs->registerCssFile($this->asset.'/highlight.js/default.css')
+		 ->registerCssFile($this->asset.'/highlight.js/github.css')
+		 ->registerCss('form', 'form { height: 100%; }')
+
+		 ->registerScriptFile($this->asset.'/js/jquery.nanoscroller.min.js', CClientScript::POS_END)
+		 ->registerScriptFile($this->asset.'/js/marked.js', CClientScript::POS_END)
+		 ->registerScriptFile($this->asset.'/highlight.js/highlight.pack.js', CClientScript::POS_END)
+
+		 ->registerScript('nano-scroller', '$(".nano").nanoScroller();')
+		 ->registerScript('marked', '
+				marked.setOptions({
+				  gfm: true,
+				  highlight: function (lang, code) {
+				    return hljs.highlightAuto(lang, code).value;
+				  },
+				  tables: true,
+				  breaks: true,
+				  pedantic: false,
+				  sanitize: true,
+				  smartLists: true,
+				  smartypants: true,
+				  langPrefix: "lang-"
+				});
+
+				$("#Content_content").keyup(function() {
+					var markdown = marked($(this).val());
+					$(".preview").html(markdown);
+					$(".nano").nanoScroller();
+				})
+		'); ?>
