5c8220685e5c0667266a6f859758214aa8a3d1c1
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-08-10 17:17:11 -0500

    Migration wasn't running

--- protected/migrations/m130723_135604_dashboard.php
+++ protected/migrations/m130723_135604_dashboard.php
@@ -5,18 +5,21 @@ class m130723_135604_dashboard extends CDbMigration
 	public function safeUp()
 	{
 		// Create the table
+		$this->execute("DROP TABLE IF EXISTS `cards`");
+		$this->execute("ALTER TABLE  `configuration` CHANGE  `key`  `key` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ;");
 		$this->execute("CREATE TABLE IF NOT EXISTS `cards` (
                       `id` int(15) NOT NULL AUTO_INCREMENT,
-                      `name` varchar(150) NOT NULL,
+                      `name` varchar(64) NOT NULL,
                       `uid` varchar(20) NOT NULL,
                       `data` TEXT,
                       `created` datetime NOT NULL,
                       PRIMARY KEY (`id`),
-                      KEY (`uid`)
+                      INDEX (`name`),
+                      KEY (`uid`),
+                      FOREIGN KEY (`name`) 
+				        REFERENCES `configuration`(`key`)
+				        ON DELETE CASCADE
                     ) ENGINE=InnoDB  DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;");
-
-		// Create a relation between configuration which stores the dashboard id
-		$this->execute("ALTER TABLE  `cards` ADD FOREIGN KEY (  `name` ) REFERENCES  `configuration` (`key`) ON DELETE CASCADE ON UPDATE NO ACTION ;");
 
 		$this->execute("ALTER TABLE  `user_metadata` CHANGE  `value`  `value` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ;");
 		$this->execute("ALTER TABLE  `configuration` CHANGE  `value`  `value` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ;");
