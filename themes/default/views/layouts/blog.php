11847b43e1561e49aaa91800e24099ddf9483077
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-08-10 11:21:40 -0500

    Re-adding Addthis

--- themes/default/views/layouts/blog.php
+++ themes/default/views/layouts/blog.php
@@ -12,6 +12,26 @@
             <?php echo CHtml::endForm(); ?>
 		</div>
 		
+		<!-- AddThis -->
+		<div class="addthis">
+			<?php if( Cii::getConfig('addThisPublisherID') != ''): ?>
+				<!-- AddThis Smart Layers BEGIN -->
+				<!-- Go to http://www.addthis.com/get/smart-layers to customize -->
+				<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=<?php echo Cii::getConfig('addThisPublisherID'); ?>"></script>
+				<script type="text/javascript">
+				  addthis.layers({
+				    'theme' : 'dark',
+				    'share' : {
+				      'position' : 'right',
+				      'numPreferredServices' : 5,
+				      'services' : 'facebook,twitter,linkedin,google_plusone_share,more'
+				    },
+				    'visible' : 'smart'
+				  });
+				</script>
+				<!-- AddThis Smart Layers END -->
+			<?php endif; ?>
+		</div>
 		<!-- Related Posts -->
 		<div class="well">
 			<h4>Related Posts</h4>
