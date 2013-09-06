53551452ec51de26d955f965000ba6cfbf3d5baa
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-09-05 19:28:07 -0500

    Using grunt to compile dashboard CSS. It's gotten too complicated now

--- protected/modules/dashboard/views/layouts/main.php
+++ protected/modules/dashboard/views/layouts/main.php
@@ -10,8 +10,8 @@
 	    <?php $bootstrap=Yii::app()->assetManager->publish(YiiBase::getPathOfAlias('application.extensions.bootstrap.assets'), true, -1, YII_DEBUG); ?>
 	    <?php Yii::app()->getClientScript()->registerMetaTag('text/html; charset=UTF-8', 'Content-Type', 'Content-Type', array(), 'Content-Type')
 				                      ->registerCssFile($bootstrap .'/css/bootstrap.min.css')
-				                      ->registerCssFile($this->asset .'/css/main.css')
-				                      ->registerCssFile($this->asset .'/font-awesome/css/font-awesome.css')
+				                      ->registerCssFile($this->asset . (YII_DEBUG ? '/css/dashboard.css' : '/css/dashboard.min.css'))
+				                      ->registerCssFile($this->asset . (YII_DEBUG ? '/font-awesome/css/font-awesome.css' : '/font-awesome/css/font-awesome.min.css'))
 				                      ->registerCssFile($this->asset .'/font-mfizz/font-mfizz.css')
 				                      ->registerCoreScript('jquery')
 				                      ->registerScriptFile($this->asset.'/js/jquery-ui.min.js', CClientScript::POS_HEAD)
@@ -19,9 +19,13 @@
 				                      ->registerScript('alert-close', '$(".close").click(function() { $(this).parent().fadeOut(); });'); ?>
 	</head>
 	<body>
-		<div class="main-container">
-			<header>
-				<div class="navigation">
+		<section class="hbox">
+			<aside class="navigation tc-container">
+				<header>
+					<?php echo CHtml::image($this->asset .'/images/ciims.png'); ?>
+					<?php echo CHtml::image(Users::model()->findByPk(Yii::app()->user->id)->gravatarImage(60), NULL, array('class' => 'user-image')); ?>
+				</header>
+				<nav>
 					<?php $this->widget('zii.widgets.CMenu', array(
 						'items' => array(
 							array('label' => Yii::t('Dashboard.views', 'Dashboard'), 'url' => $this->createUrl('/dashboard'), 'active' => $this->id == 'default'),
@@ -29,16 +33,11 @@
 							array('label' => Yii::t('Dashboard.views', 'Settings'), 'url' => $this->createUrl('/dashboard/settings'), 'active' => !in_array($this->id, array('default', 'content'))),
 						)
 					)); ?>
-				</div>
-				<div class="user">
-					<!-- TODO: Proper Link to... ??? -->
-					<?php echo CHtml::link(Yii::app()->user->displayName); ?>
-					<?php echo CHtml::image(Users::model()->findByPk(Yii::app()->user->id)->gravatarImage(), NULL, array('class' => 'user-image')); ?>
-					<?php echo CHtml::tag('span', array('class' => 'options')); ?>
-				</div>
-				<div class="clearfix"></div>
-			</header>
-			<main>
+				</nav>
+				<footer></footer>
+			</aside>
+				
+			<main class="tc-container">
 				<?php $this->widget('bootstrap.widgets.TbAlert', array(
 	                  'block'=>true,
 	                  'fade'=>true,
@@ -46,9 +45,11 @@
 	              ));?>
 				<?php echo $content; ?>
 			</main>
+
+			<div class="clearfix"></div>
+			<footer>
+			</footer>
+			<?php echo CHtml::tag('span', array('style' => 'display:none', 'value' => $this->createUrl('/dashboard'), 'id' => 'dashboard-endpoint'), NULL); ?>
 		</div>
-		<footer>
-		</footer>
-		<?php echo CHtml::tag('span', array('style' => 'display:none', 'value' => $this->createUrl('/dashboard'), 'id' => 'dashboard-endpoint'), NULL); ?>
 	<body>
 </html>
