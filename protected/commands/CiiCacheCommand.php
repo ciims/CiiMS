0db410182437f9f3aeb194e8fe304fb35a7f1a1f
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-10-15 18:49:22 -0500

    Adding a way to flush the cache on the demo deployments since that seems to be the biggest issue right now

---
+++ protected/commands/CiiCacheCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+class CiiCacheCommand extends CConsoleCommand
+{
+	public function run($args=array())
+	{
+		if (!isset($args[0]))
+			return $this->showCommands();
+		
+		switch ($args[0])
+		{
+			case "flush":
+				Yii::app()->cache->flush();
+			default:
+				$this->showCommands();
+		}		
+	}
+
+	private function showCommands()
+	{
+		$this->log("CiiCacheCommand: A cache helper to interacte with CiiMS's cache");
+		$this->log("===============================================================");
+		$this->log("Usage:");
+		$this->log("    php protected/yiic.php ciicache [arg1] [arg2] [arg3] [...] [argn]\n");
+		$this->log("Arguments:");
+		$this->log("    flush           Completely flushes the local cache. Items will be rebuilt as necessary");
+
+		$this->log();
+	}
+
+	/**
+	 * Simple logging command to make life easier
+	 * @param  string $message The message we want to output
+	 */
+	private function log($message="")
+	{
+		echo $message . "\n";
+	}
+}
