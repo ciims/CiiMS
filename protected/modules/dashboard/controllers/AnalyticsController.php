b6e0de386ff09390e501adf4d9b574382e6f9ff8
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2013-10-26 10:36:33 -0500

    Tightening up permissions in dashboard

--- protected/modules/dashboard/controllers/AnalyticsController.php
+++ protected/modules/dashboard/controllers/AnalyticsController.php
@@ -2,6 +2,24 @@
 
 class AnalyticsController extends CiiSettingsController
 {
+	/**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+            return array(
+                    array('allow',  // allow authenticated admins to perform any action
+                            'users'=>array('@'),
+                            'expression'=>'Yii::app()->user->role==6||Yii::app()->user->role==9'
+                    ),
+                    array('deny',  // deny all users
+                            'users'=>array('*'),
+                    ),
+            );
+    }
+    
 	public function actionIndex()
 	{
 		$this->render('index');
--- protected/modules/dashboard/controllers/UsersController.php
+++ protected/modules/dashboard/controllers/UsersController.php
@@ -3,6 +3,24 @@
 class UsersController extends CiiSettingsController
 {
 	/**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+            return array(
+                    array('allow',  // allow authenticated admins to perform any action
+                            'users'=>array('@'),
+                            'expression'=>'Yii::app()->user->role==6||Yii::app()->user->role==9'
+                    ),
+                    array('deny',  // deny all users
+                            'users'=>array('*'),
+                    ),
+            );
+    }
+
+	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
