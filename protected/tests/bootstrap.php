870aa4ab722517e87bd1e3fc077cdab3208d2a60
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-03-26 21:29:28 -0500

    Lazy typo

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', true);
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
 // change the following paths if necessary
 $yiit=__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
