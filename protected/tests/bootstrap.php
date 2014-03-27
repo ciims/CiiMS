298c58172e221a9833420cf68aa7ca25356cac62
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-03-26 21:26:43 -0500

    typo fix

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yiit=__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiit.php');
+$yiit=__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 $CiiMSTestConfig = require_once($config);
