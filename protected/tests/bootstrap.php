d18478a64a9fa3734b522b451e7059624b73cace
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-03-26 21:32:02 -0500

    Lazy typo

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -5,7 +5,7 @@ ini_set('display_errors', true);
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
 // change the following paths if necessary
-$yiit=__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiit.php';
+$yiit=__DIR__.DS.'..'.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 $CiiMSTestConfig = require_once($config);
