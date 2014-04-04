585b969ce32c82e0e4181f668564ff1bd3980daf
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-04-04 13:22:04 -0500

    Updating the default theme's composer.json. Probably going to split it off as a git ref

--- themes/default/Theme.php
+++ themes/default/Theme.php
@@ -1,8 +1,9 @@
 <?php
 
+require __DIR__.DS.'vendor'.DS.'autoload.php';
 Yii::import('application.modules.dashboard.components.CiiSettingsModel');
 class Theme extends CiiThemesModel
 {
-    // @var string  The theme name
+	// @var string  The theme name
 	public $theme = 'default';
 }
