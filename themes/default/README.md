8b56ad41f3d64117555e03f19e063099075bb6fc
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-04-04 14:11:46 -0500

    Removing default theme to reconfigure as subtree

--- themes/default/README.md
+++
@@ -1,3 +0,0 @@
-## CiiMS Default 2014 Theme
-
-This is the default theme that comes prebundled with CiiMS
--- themes/default/Theme.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__.DS.'vendor'.DS.'autoload.php';
-Yii::import('application.modules.dashboard.components.CiiSettingsModel');
-class Theme extends CiiThemesModel
-{
-	// @var string  The theme name
-	public $theme = 'default';
-}
