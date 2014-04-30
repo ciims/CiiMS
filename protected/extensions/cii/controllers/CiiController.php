0b4fefce4885e61031daf6b0d950cbe7d0c8454d
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-04-29 20:18:21 -0500

    broke stuff with themes, had to refix it

--- protected/extensions/cii/controllers/CiiController.php
+++ protected/extensions/cii/controllers/CiiController.php
@@ -231,12 +231,10 @@ class CiiController extends CController
         if ($isOffline && $this->id == 'site')
         {
             if (!in_array($action->id, array('login', 'logout', 'error', 'sitemap', 'migrate')))
-                    throw new CHttpException(403, Yii::t('ciims.controllers.Cii', 'This site is currently disabled. Please check back later.'));
-        }
-        else if (!(isset($this->module) && $this->module->getName() == "dashboard"))
-        {
                 throw new CHttpException(403, Yii::t('ciims.controllers.Cii', 'This site is currently disabled. Please check back later.'));
         }
+        else if (!(isset($this->module) && $this->module->getName() == "dashboard"))
+            throw new CHttpException(403, Yii::t('ciims.controllers.Cii', 'This site is currently disabled. Please check back later.'));
 
         return;
     }
