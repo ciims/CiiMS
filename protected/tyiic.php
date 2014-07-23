65fb225ed3554a175d596bba1b89fa16b553711d
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2014-07-23 15:12:34 -0500

    composer, travis, and yiic update

--- protected/tyiic.php
+++ protected/tyiic.php
@@ -1,36 +1,28 @@
 <?php
 error_reporting(-1);
-ini_set('display_errors', true);
+ini_set('display_errors', 'true');
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 
-function mergeArray($a,$b)
-{
-    $args=func_get_args();
-    $res=array_shift($args);
-    while(!empty($args))
-    {
-        $next=array_shift($args);
-        foreach($next as $k => $v)
-        {
-            if(is_integer($k))
-                isset($res[$k]) ? $res[]=$v : $res[$k]=$v;
-            elseif(is_array($v) && isset($res[$k]) && is_array($res[$k]))
-                $res[$k]=mergeArray($res[$k],$v);
-            else
-                $res[$k]=$v;
-        }
-    }
-    return $res;
-}
-
-// Load the configs
-$config = require_once __DIR__.DS.'config'.DS.'test.php';
-$defaultConfig = require_once  __DIR__.DS.'config'.DS.'main.default.php';
-
-$config = mergeArray($defaultConfig, $config);
-unset($config['components']['request']);
-unset($config['components']['user']);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 // Include the composer dependencies
 require(__DIR__.DS.'..'.DS.'vendor'.DS.'autoload.php');
-require(__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yiic.php');
+require(__DIR__.DS.'..'.DS.'vendor'.DS.'yiisoft'.DS.'yii'.DS.'framework'.DS.'yii.php');
+
+Yii::setPathOfAlias('vendor', __DIR__.DS.'..'.DS.'vendor'.DS);
+
+// Load the configs
+$config = require __DIR__.DS.'config'.DS.'test.php';
+$defaultConfig = require __DIR__.DS.'config'.DS.'main.default.php';
+
+$config = CMap::mergeArray($defaultConfig, $config);
+
+unset($config['components']['user']);
+
+$app=Yii::createConsoleApplication($config);
+$app->commandRunner->addCommands(YII_PATH.'/cli/commands');
+$env=@getenv('YII_CONSOLE_COMMANDS');
+if(!empty($env))
+   $app->commandRunner->addCommands($env);
+
+$app->run();
