772a7d78be67d4ed361dfc54b62969d2115ff7cf
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-03-30 21:03:02 +0000

    Moving rendering function into EmailSettings so that EmailSettings and render from codeception

--- protected/models/settings/EmailSettings.php
+++ protected/models/settings/EmailSettings.php
@@ -113,7 +113,7 @@ class EmailSettings extends CiiSettingsModel
 
 		$mail->SetFrom($notifyUser->email, $notifyUser->username);
 		$mail->Subject = $subject;
-		$mail->MsgHTML(Yii::app()->controller->renderFile(Yii::getPathOfAlias($viewFile).'.php', $content, $return, $processOutput));
+		$mail->MsgHTML($this->renderFile(Yii::getPathOfAlias($viewFile).'.php', $content, $return, $processOutput));
 		$mail->AddAddress($user->email, $user->username);
 
 		try
@@ -130,5 +130,51 @@ class EmailSettings extends CiiSettingsModel
 		}
 
 		return false;
+	}
+
+	/**
+	 * Renders a view file.
+	 *
+	 * @param string $viewFile view file path
+	 * @param array $data data to be extracted and made available to the view
+	 * @param boolean $return whether the rendering result should be returned instead of being echoed
+	 * @return string the rendering result. Null if the rendering result is not required.
+	 * @throws CException if the view file does not exist
+	 */
+	private function renderFile($viewFile,$data=null,$return=false)
+	{
+		if(($renderer=Yii::app()->getViewRenderer())!==null && $renderer->fileExtension==='.'.CFileHelper::getExtension($viewFile))
+			$content=$renderer->renderFile($this,$viewFile,$data,$return);
+		else
+			$content=$this->renderInternal($viewFile,$data,$return);
+		
+		return $content;
+	}
+
+	/**
+	 * Renders a view file.
+	 * This method includes the view file as a PHP script
+	 * and captures the display result if required.
+	 * @param string $_viewFile_ view file
+	 * @param array $_data_ data to be extracted and made available to the view file
+	 * @param boolean $_return_ whether the rendering result should be returned as a string
+	 * @return string the rendering result. Null if the rendering result is not required.
+	 */
+	private function renderInternal($_viewFile_,$_data_=null,$_return_=false)
+	{
+	    // we use special variable names here to avoid conflict when extracting data
+	    if(is_array($_data_))
+	        extract($_data_,EXTR_PREFIX_SAME,'data');
+	    else
+	        $data=$_data_;
+	    if($_return_)
+	    {
+	        ob_start();
+	        ob_implicit_flush(false);
+	        require($_viewFile_);
+	        return ob_get_clean();
+	    }
+	    else
+	        require($_viewFile_);
 	}
 }
